ebe0c94c0cc5fa835238f64d57cd2df7d5f220a1
roykishan8 <35493574+roykishan8@users.noreply.github.com>
2021-09-27 12:48:34 +0530

    Add files via upload

---
+++ TaskManagement/src/app/AdminModule/MyModule.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { FirstComponent } from "./FirstComponent";
+
+@NgModule({
+    declarations: [FirstComponent],
+    imports:[],
+    exports: [FirstComponent]
+})
+
+export class MyModule{
+
+}
